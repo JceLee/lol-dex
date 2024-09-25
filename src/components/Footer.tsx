5a7a3af78a770344ebfa9e0bb7fee49c93ddf302
jcelee <kangmin@pthreemedia.com>
2024-09-25 13:05:27 +0900

    rotation page update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 // components/Footer.tsx
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 p-4 mt-8 fixed bottom-0">
+    <footer className="bg-gray-800 p-4 mt-8 fixed bottom-0 w-full">
       <div className="container mx-auto text-center text-white text-sm">
         [Your Product Name] is not endorsed by Riot Games and does not reflect
         the views or opinions of Riot Games or anyone officially involved in
--- src/utils/riotApi.ts
+++ src/utils/riotApi.ts
@@ -1,6 +1,8 @@
 // utils/riotApi.ts
+const NEXT_PUBLIC_HOST = process.env.NEXT_PUBLIC_HOST;
+
 export async function getChampionRotation() {
-  const res = await fetch("/api/rotation");
+  const res = await fetch(`${NEXT_PUBLIC_HOST}/api/rotation`);
   if (!res.ok) {
     throw new Error("로테이션 데이터를 가져오는 데 실패했습니다.");
   }
